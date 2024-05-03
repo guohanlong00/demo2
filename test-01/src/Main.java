860f925399fb2ed55c4c8626bb5d12acefe4b8c6
daniel <daniel.com>
2024-05-03 19:52:08 +0800

    hot-fix-two 第一次添加

--- test-01/src/Main.java
+++ test-01/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("主分支 你好世界2");
         System.out.println("你好世界2");
         System.out.println("hot-fix第三次添加");
+        System.out.println("hot-fix-two 第一次添加");
     }
 }
