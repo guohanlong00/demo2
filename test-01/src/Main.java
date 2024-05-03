96765218ab2030b6789d9dd6ebdde7165703c731
daniel <daniel.com>
2024-05-03 19:21:58 +0800

    添加你好世界第二次

--- test-01/src/Main.java
+++ test-01/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("你好世界");
+        System.out.println("你好世界2");
     }
 }
